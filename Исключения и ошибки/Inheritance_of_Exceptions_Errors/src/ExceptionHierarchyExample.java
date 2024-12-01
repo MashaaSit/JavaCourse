e2c9a8e6deadf32e2a6c09348e5cefd7d1de883c
MashaaSit <129902332+MashaaSit@users.noreply.github.com>
2024-12-01 14:20:02 +0300

    Update ExceptionHierarchyExample.java

--- Исключения и ошибки/Inheritance_of_Exceptions_Errors/src/ExceptionHierarchyExample.java
+++ Исключения и ошибки/Inheritance_of_Exceptions_Errors/src/ExceptionHierarchyExample.java
@@ -1,18 +1,18 @@
-//class Exc1 extends Exception {}
-//class Exc2 extends Exc1 {}
-//class Exc3 extends Ex2 {}
-//
-//public class ExceptionHierarchyExample {
-//    public static void main(String[] args) {
-//        try {
-//            // В этом примере выбрасываем исключение Ex3
-//            throw new Exc3();
-//        } catch (Exc3 e) {
-//            System.out.println("Поймано исключение типа Ex3");
-//        } catch (Exc2 e) {
-//            System.out.println("Поймано исключение типа Ex2");
-//        } catch (Exc1 e) {
-//            System.out.println("Поймано исключение типа Ex1");
-//        }
-//    }
-//}
+class Exc1 extends Exception {}
+class Exc2 extends Exc1 {}
+class Exc3 extends Exc2 {}
+
+public class ExceptionHierarchyExample {
+    public static void main(String[] args) {
+        try {
+            // В этом примере выбрасываем исключение Ex3
+            throw new Exc3();
+        } catch (Exc3 e) {
+            System.out.println("Поймано исключение типа Ex3");
+        } catch (Exc2 e) {
+            System.out.println("Поймано исключение типа Ex2");
+        } catch (Exc1 e) {
+            System.out.println("Поймано исключение типа Ex1");
+        }
+    }
+}
